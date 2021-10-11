7f7d8255755ab818a9324b761e5b4ab25a55ea66
nguyenbinhit <69001423+nguyenbinhit@users.noreply.github.com>
2021-10-11 22:00:55 +0700

    create dashboard

---
+++ Views/dashboard.php
@@ -0,0 +1,91 @@
+<?php require_once __DIR__. "/Layout/header.php" ?>
+
+    <!-- ========== MAIN CONTENT ========== -->
+    <main id="content" role="main" class="main pointer-event">
+        <!-- Content -->
+        <div class="content container-fluid">
+            <!-- Page Header -->
+            <div class="page-header">
+                <div class="row align-items-center">
+                    <div class="col-sm mb-2 mb-sm-0">
+                        <h1 class="page-header-title">Bảng thống kê</h1>
+                    </div>
+                </div>
+            </div>
+            <!-- End Page Header -->
+
+            <!-- Stats -->
+            <div class="row gx-2 gx-lg-3">
+                <div class="col-sm-6 col-lg-3 mb-3 mb-lg-5">
+                    <!-- Card -->
+                    <a class="card card-hover-shadow h-100" href="#">
+                        <div class="card-body">
+                            <h6 class="card-subtitle">Tổng số tài khoản nhân viên quản lý</h6>
+
+                            <div class="row align-items-center gx-2 mb-1 mt-5">
+                                <div class="col-6">
+                                    <span class="card-title h1">72,540</span>
+                                </div>
+                            </div>
+                            <!-- End Row -->
+                        </div>
+                    </a>
+                    <!-- End Card -->
+                </div>
+
+                <div class="col-sm-6 col-lg-3 mb-3 mb-lg-5">
+                    <!-- Card -->
+                    <a class="card card-hover-shadow h-100" href="#">
+                        <div class="card-body">
+                            <h6 class="card-subtitle">Tổng số tài khoản giáo viên</h6>
+
+                            <div class="row align-items-center gx-2 mb-1 mt-5">
+                                <div class="col-6">
+                                    <span class="card-title h1">29.4%</span>
+                                </div>
+                            </div>
+                            <!-- End Row -->
+                        </div>
+                    </a>
+                    <!-- End Card -->
+                </div>
+
+                <div class="col-sm-6 col-lg-3 mb-3 mb-lg-5">
+                    <!-- Card -->
+                    <a class="card card-hover-shadow h-100" href="#">
+                        <div class="card-body">
+                            <h6 class="card-subtitle">Tổng số tài khoản học viên</h6>
+
+                            <div class="row align-items-center gx-2 mb-1 mt-5">
+                                <div class="col-6">
+                                    <span class="card-title h1">56.8%</span>
+                                </div>
+                            </div>
+                            <!-- End Row -->
+                        </div>
+                    </a>
+                    <!-- End Card -->
+                </div>
+
+                <div class="col-sm-6 col-lg-3 mb-3 mb-lg-5">
+                    <!-- Card -->
+                    <a class="card card-hover-shadow h-100" href="#">
+                        <div class="card-body">
+                            <h6 class="card-subtitle">Tổng các khoá học</h6>
+
+                            <div class="row align-items-center gx-2 mb-1 mt-5">
+                                <div class="col-6">
+                                    <span class="card-title h1">56.8%</span>
+                                </div>
+                            </div>
+                            <!-- End Row -->
+                        </div>
+                    </a>
+                    <!-- End Card -->
+                </div>
+            </div>
+            <!-- End Stats -->
+        </div>
+        <!-- End Content -->
+
+<?php require_once __DIR__. "/Layout/footer.php" ?>
